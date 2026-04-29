1d8ebfde622ee192faca4ea4ab4ff73655e58ce9
ArtemKurasov <kurasova@berea.edu>
2026-04-29 10:47:16 -0400

    Changed the placement of Report UI Elements

--- code/Report.py
+++ code/Report.py
@@ -36,30 +36,30 @@ class Report:
 
     def update_report(self, screen, person):
 
-        self.name_label = ctk.CTkLabel(screen, text = "Name: ")
-        self.zodiac_sign_label = ctk.CTkLabel(screen, text = "Element: ")
-        self.element_label = ctk.CTkLabel(screen, text = "Zodiac Sign: ")
-        self.kua_number_label = ctk.CTkLabel(screen, text = "Kua Number: ")
+        self.name_label = ctk.CTkLabel(screen, text = "Name: ", font = ("Arial", 15, "bold"))
+        self.zodiac_sign_label = ctk.CTkLabel(screen, text = "Element: ", font = ("Arial", 15, "bold"))
+        self.element_label = ctk.CTkLabel(screen, text = "Zodiac Sign: ", font = ("Arial", 15, "bold"))
+        self.kua_number_label = ctk.CTkLabel(screen, text = "Kua Number: ", font = ("Arial", 15, "bold"))
 
-        self.name = ctk.CTkLabel(screen, text = person.name)
-        self.zodiac_sign = ctk.CTkLabel(screen, text = person.zodiac_sign)
-        self.element = ctk.CTkLabel(screen, text = person.element)
-        self.kua_number = ctk.CTkLabel(screen, text = person.kua_number)
+        self.name = ctk.CTkLabel(screen, text = person.name, font = ("Arial", 15))
+        self.zodiac_sign = ctk.CTkLabel(screen, text = person.zodiac_sign, font = ("Arial", 15))
+        self.element = ctk.CTkLabel(screen, text = person.element, font = ("Arial", 15))
+        self.kua_number = ctk.CTkLabel(screen, text = person.kua_number, font = ("Arial", 15))
 
         #self.zodiac_sign_image = ctk.CTkImage(screen)
         #self.element_image = ctk.CTkImage(screen)
 
         self.personal_description = ctk.CTkLabel(screen, text = person.description)
 
-        self.name_label.place(x = 500, y = 10)
-        self.zodiac_sign_label.place(x = 500, y = 20)
-        self.element_label.place(x = 500, y = 30)
-        self.kua_number_label.place(x = 500, y = 40)
+        self.name_label.place(x = 600, y = 50)
+        self.zodiac_sign_label.place(x = 600, y = 75)
+        self.element_label.place(x = 600, y = 100)
+        self.kua_number_label.place(x = 600, y = 125)
 
-        self.name.place(x = 500, y = 50)
-        self.zodiac_sign.place(x = 500, y = 60)
-        self.element.place(x = 500, y = 70)
-        self.kua_number.place(x = 500, y = 80)
+        self.name.place(x = 700, y = 50)
+        self.zodiac_sign.place(x = 700, y = 75)
+        self.element.place(x = 700, y = 100)
+        self.kua_number.place(x = 700, y = 125)
 
         #self.zodiac_sign_image.place(x = 500, y = 90)
         #self.element_image.place(x = 500, y = 100)
